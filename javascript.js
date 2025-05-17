b859c4bb31e5806b958748c05741b108342d6d43
EyalA6104 <152385011+EyalA6104@users.noreply.github.com>
2025-05-17 16:12:21 +0300

    added basic math operators functions

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function add (a, b) {
+    return a + b;
+};
+console.log(add(5, 8));
+
+function subtract (a, b) {
+    return a - b;
+};
+console.log(subtract(6, 2));
+
+function multiply (a, b) {
+    return a * b;
+};
+console.log(multiply(5, 5));
+
+function divide (a, b) {
+    return a / b;
+};
+console.log(divide(8, 4));
