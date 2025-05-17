ae7e84602b7049eeae3a859168c40a8cbb2e7b59
EyalA6104 <152385011+EyalA6104@users.noreply.github.com>
2025-05-17 16:52:13 +0300

    added operate function

--- javascript.js
+++ javascript.js
@@ -1,19 +1,38 @@
-function add (a, b) {
+function add(a, b) {
     return a + b;
 };
 console.log(add(5, 8));
 
-function subtract (a, b) {
+function subtract(a, b) {
     return a - b;
 };
 console.log(subtract(6, 2));
 
-function multiply (a, b) {
+function multiply(a, b) {
     return a * b;
 };
 console.log(multiply(5, 5));
 
-function divide (a, b) {
+function divide(a, b) {
     return a / b;
 };
 console.log(divide(8, 4));
+
+let firstNum = 1
+let operator = "+"
+let secondNum = 2
+
+function operate (operator, a, b) {
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return subtract(a, b);
+    } else if (operator === "*") {
+        return multiply(a, b);
+    } else if (operator === "/") {
+        return divide(a, b);
+    } else {
+        return "Error";
+    }
+};
+console.log(operate("*", 6, 2))
